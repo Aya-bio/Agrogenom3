225c3591a2f20bcff60a723d2a5a9c2404ae3781
Aya-bio <ayamohame1995@gmail.com>
2026-05-02 04:42:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,3 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-
-function figmaAssetResolver() {
-  return {
-    name: 'figma-asset-resolver',
-    resolveId(id) {
-      if (id.startsWith('figma:asset/')) {
-        const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
-      }
-    },
-  }
-}
-
 export default defineConfig({
   plugins: [
     figmaAssetResolver(),
@@ -30,6 +13,5 @@ export default defineConfig({
 
   assetsInclude: ['**/*.svg', '**/*.csv'],
 
-  // 🔥 مهم جدًا لGitHub Pages
   base: '/Agrogenom3/',
 })
